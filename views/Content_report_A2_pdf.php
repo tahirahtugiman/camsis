994c6a3ac078607dd60d06fae1346a4ee2eba3ba
syafiqwahid <syafiqwahid90@gmail.com>
2018-06-04 08:27:24 +0800

    task A2 Work Order Listing
    *task jumaat
    generate report a2 listing
    *submit on 4.06.2017

--- views/Content_report_A2_pdf.php
+++ views/Content_report_A2_pdf.php
@@ -11,46 +11,55 @@
 			<td colspan="5"><h3>A2 LISTING - <?=date('F', mktime(0, 0, 0, $month, 10))?> <?=$year?> - <?php echo $this->session->userdata('usersessn');?>  ( <?php if ($this->input->get('grp') == ''){echo 'ALL'; }else{ echo 'Group '.$this->input->get('grp');} ?> )</h3></td>
 		</tr>
 	</table>
-	<table class="tftable" border="1" style="text-align:center;width:100%; font-size:7px;" cellpadding="5" cellspacing="0">
-		<tr>
+	
+	<table class="tftable" border="1" style="text-align:center;width:100%; font-size:7px;" cellpadding="5" cellspacing="0" >
+		<tr nobr="true">
 			<th rowspan="2" style="width:19px;">No</th>
-			<th rowspan="2">A2 Scheduled Date</th>
+			<th rowspan="2">Work Order Date</th>
 			<th rowspan="2">A2 Work Order</th>
 			<th rowspan="2">Asset No</th>	
 			<th rowspan="2" style="width:8%;">Equipment Name</th>
 			<th rowspan="2">UDP</th>
-			<th rowspan="2">Freq</th>
 			<th rowspan="2">Status</th>
 			<th colspan="2">Test</th>
-			<th rowspan="2">Completion Date</th>
+			<th rowspan="2">Complete Date</th>
 			<th rowspan="2">Remark</th>
-			<th rowspan="2">Visit Date</th>
+			<th rowspan="2">Schedule Date</th>
 			<th rowspan="2">Reschedule Date</th>
 			<th rowspan="2">Deparment (Location Code)</th>
 			<th rowspan="2">Asset Group</th>
 		</tr>
-		<tr>
+		<tr nobr="true">
 			<th>S</th>
 			<th>P</th>
 		</tr>
-		<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		
+			<?php $numrow = 1; foreach($recordrq as $row):?>
+<?php echo ($numrow%2==0) ? '<tr class="ui-color-color-color">' : '<tr nobr="true">'; ?>
+		<td><?= $numrow ?></td>
+		<td><?= ($row->D_date) ?  date("d/m/Y",strtotime($row->D_date)) : 'N/A' ?></td>
+		<td><?=($row->V_Request_no) ? $row->V_Request_no : 'N/A' ?></td>
+		<td><?=(($row->V_Asset_no) && $row->V_Asset_no != 'N/A') ? $row->v_tag_no : 'N/A' ?></td>
+		<td><?= ($row->V_Asset_name) ? $row->V_Asset_name : 'N/A' ?></td>
+		<td><?= ($row->V_User_dept_code) ? $row->V_User_dept_code : 'N/A' ?></td>
+		<td><?= ($row->V_request_status) ? $row->V_request_status : 'N/A' ?></td>
+	    <td><?= 'N/A' ?></td>
+	    <td><?= 'N/A' ?></td>
+		<td><?= ($row->d_Date) ? date("d/m/Y",strtotime($row->d_Date)) : 'N/A' ?></td>
+		<td><?= ($row->v_ActionTaken) ? $row->v_ActionTaken : 'N/A' ?></td>
+		<td><?= ($row->v_closeddate) ? date("d/m/Y",strtotime($row->v_closeddate)) : 'N/A' ?></td>
+		<td><?= 'N/A' ?></td>
+		<td><?= ($row->v_UserDeptDesc) ? $row->v_location_name.' ('.$row->v_location_code.')' : 'N/A' ?></td>
+		<?php if ($this->input->get('broughtfwd') != '') { ?>
+			<td><?= ($row->V_Asset_WG_code) ? $row->V_Asset_WG_code : 'N/A' ?></td>
+			<?php } else { ?>
+			<td><?= ($row->v_asset_grp) ? $row->v_asset_grp : 'N/A' ?></td>
+			<?php } ?>
         </tr>
+		<?php $numrow++; ?>
+		<?php endforeach;?>
 	</table>
+
 	</body>
 </html>
 <?php include 'pdf_footer.php'?>
