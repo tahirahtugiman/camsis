6ea73aa471166ab07e5a9ed4c8f7ca7f06b9ecdf
syafiqwahid <syafiqwahid90@gmail.com>
2018-06-26 09:01:31 +0800

    *hide mrin tab
    *26/06/2018

--- views/content_pr_tab.php
+++ views/content_pr_tab.php
@@ -1,8 +1,8 @@
 <tr class="ui-left_web">
 	<?= ($this->input->get('tab') == '0') ? '<td class="ui-highlight" align="center" colspan="0" style=" height:30px; width:25%;">' : '<td class="ui-content-menu-desk-color" align="center" colspan="0" style=" height:30px; width:25%;">'?>
 	<?php echo anchor ('Procurement/e_pr?tab=0'.'&y='.$year.'&m='.$month, 'MRIN TO PR Approval'); ?></td>
-	<?= ($this->input->get('tab') == '1') ? '<td class="ui-highlight" align="center" colspan="0" style="height:30px; width:25%;">' : '<td class="ui-content-menu-desk-color" align="center" colspan="0" style="width:25%;">'?>
-	<?php echo anchor ('Procurement/e_pr?tab=1'.'&y='.$year.'&m='.$month, 'Purchase Request'); ?></td>
+	<!---<?= ($this->input->get('tab') == '1') ? '<td class="ui-highlight" align="center" colspan="0" style="height:30px; width:25%;">' : '<td class="ui-content-menu-desk-color" align="center" colspan="0" style="width:25%;">'?>
+	<?php echo anchor ('Procurement/e_pr?tab=1'.'&y='.$year.'&m='.$month, 'Purchase Request'); ?></td> -->
 	<?= ($this->input->get('tab') == '2') ? '<td class="ui-highlight" align="center" colspan="0" style="width:25%;">' : '<td class="ui-content-menu-desk-color" align="center" colspan="0" style="width:25%;">'?>
 	<?php echo anchor ('Procurement/e_pr?tab=2'.'&y='.$year.'&m='.$month, 'Purchase Order'); ?></td>
 </tr>
@@ -12,7 +12,7 @@
 
 
 if (!is_null($this->input->get('desk'))){
-	if ($this->input->get('desk') == 0){ 
+	/*if ($this->input->get('desk') == 0){ 
 		echo "<table class='ui-mobile-content-header' border='0' align='center'>";
 		echo "<tr>";
 		echo "<td style='width:20px;'>";
@@ -56,6 +56,35 @@ if (!is_null($this->input->get('desk'))){
 		echo "</td>"; 
 		echo "</tr>";
 		echo "</table>";
+	}*/
+	if( $this->input->get('tab') == 0 ){
+		echo "<div class='divmenu'>";
+		echo "<div class='divmenuleft'>";
+		echo "</div>";
+		echo $tulis;
+		echo "<div class='divmenuright'>";
+		echo anchor ('Procurement/e_pr?tab=1'.'&y='.$year.'&m='.$month, '<span class="icon-arrow-right"></span>');
+		echo "</div>";
+		echo "</div>";
+	}elseif( $this->input->get('tab') == 1 ){
+		echo "<div class='divmenu'>";
+		echo "<div class='divmenuleft'>";
+		echo anchor ('Procurement/e_pr?tab=0'.'&y='.$year.'&m='.$month, '<span class="icon-arrow-left"></span>');
+		echo "</div>";
+		echo $tulis;
+		echo "<div class='divmenuright'>";
+		echo anchor ('Procurement/e_pr?tab=2'.'&y='.$year.'&m='.$month, '<span class="icon-arrow-right"></span>');
+		echo "</div>";
+		echo "</div>";
+	}elseif( $this->input->get('tab') == 2 ){
+		echo "<div class='divmenu'>";
+		echo "<div class='divmenuleft'>";
+		echo anchor ('Procurement/e_pr?tab=1'.'&y='.$year.'&m='.$month, '<span class="icon-arrow-left"></span>');
+		echo "</div>";
+		echo $tulis;
+		echo "<div class='divmenuright'>";
+		echo "</div>";
+		echo "</div>";
 	}
 }
 ?>
