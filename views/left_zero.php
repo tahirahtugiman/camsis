67a2802f53a20665bcb3b9b0b1d930c8cbb9eadb
syafiqwahid <syafiqwahid90@gmail.com>
2018-07-24 12:32:35 +0800

    *DELETE OLD FILES
    *delete files lama

--- views/left_zero.php
+++
@@ -1,6 +0,0 @@
-<body class="body-screen">
-	
-	<div class="ui-left-screen">
-<?php echo anchor ('contentcontroller/content/'.$this->session->userdata('usersess'), '<p align="center" style="background:#B3130A;  font-size:20px; font-weight:bold; padding-top:15px; padding-bottom:15px; margin-top:0px; color:white;">Facility Management Information<br /> System</p>'); ?>
-
-		</div>	
