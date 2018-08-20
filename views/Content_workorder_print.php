6d4657cc07d5a2c37d794754fb1b4dc78ac39601
BuzzLee93 <buzzlee199@yahoo>
2018-08-20 16:13:07 +0800

    update workorder print n work order complaint
    change html view

--- views/Content_workorder_print.php
+++ views/Content_workorder_print.php
@@ -17,7 +17,7 @@
 	<div class="">
 		<table class="tbl-wo" border="1" align="center" style="border: 1px solid black;margin-top:5px;">
 			<tr>
-				<td style="padding-left:0px; width:120px;" align="center"><img src="<?php echo base_url(); ?>images/penmedic2.png" style="width:100px; height:40px;"/></td>
+				<td style="padding-left:0px; width:120px;" align="center"><img src="<?php echo base_url(); ?>images/penmedic3.png" style="width:100px; height:40px;"/></td>
 				<td>
 					<table class="tbl-wo" border="0" align="center">
 						<tr>
@@ -32,10 +32,18 @@
 						<tr>
 							<td align="center"><b style="text-transform: uppercase;">
 							<?php if(($this->session->userdata('usersess') == 'SEC') or ($this->session->userdata('usersess') == 'HKS')){?>
-							<?php }else{ ?>
 							REQUEST
+							<?php }else{ ?>
+							SERVICE AND REQUEST 
 							<?php } ?>
-							Work ORDER</b></td>
+							Work ORDER
+							<?php if(($this->session->userdata('usersess') == 'SEC') or ($this->session->userdata('usersess') == 'HKS') ){?>
+								(SCS/HKS)
+							<?php } ?>
+							<?php if(($this->session->userdata('usersess') == 'FES') or ($this->session->userdata('usersess') == 'BES') ){?>
+								(FES/BES)
+							<?php } ?>
+							</b></td>
 						</tr>
 					</table>
 				</td>
@@ -48,31 +56,31 @@
 				<td style="padding:1px; width:15%; font-size: 12px;">SERVICE TYPE : </td>
 				<?php if(($this->session->userdata('usersess') == 'SEC')){?>
 				<td style="padding:1px; width:80px; font-size: 13px;">
-					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> SEC
+					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> SCS
 				</td>
 				<td style="padding:1px; width:80px; font-size: 12px;"> 
 					<div class="box2"></div> HKS
 				</td>
 				<?php }elseif(($this->session->userdata('usersess') == 'HKS')){ ?>
 				<td style="padding:1px; width:80px; font-size: 12px;">
-					<div class="box2"></div> SEC
+					<div class="box2"></div> SCS
 				</td>
 				<td style="padding:1px; width:80px; font-size: 12px;">
 					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> HKS			
 				</td>
 				<?php }elseif(($this->session->userdata('usersess') == 'BES')){ ?>
-				<td style="padding:1px; width:80px; font-size: 12px;">
-					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> BES
-				</td>
 				<td style="padding:1px; width:80px; font-size: 12px;"> 
 					<div class="box2"></div> FES
 				</td>
+				<td style="padding:1px; width:80px; font-size: 12px;">
+					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> BES
+				</td>
 				<?php }elseif(($this->session->userdata('usersess') == 'FES')){ ?>
 				<td style="padding:1px; width:80px; font-size: 12px;">
-					<div class="box2"></div> BES
+					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> FES			
 				</td>
 				<td style="padding:1px; width:80px; font-size: 12px;">
-					<div class="box2"><img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/></div> FES			
+					<div class="box2"></div> BES
 				</td>
 				<?php } ?>
 				<td style="padding-left:45%; width:400px; font-size: 12px;"> Date : <?= date("d/m/Y")?> </td>
@@ -90,12 +98,13 @@
 		</table>-->
 		<table class="tbl-wo" border="1" align="center" style="border: 1px solid black;">
 			<tr>
-			<td style="width:50%; padding:1px;"><table class="tbl-wo-1" border="0" align="left">
+				<td style="width:50%; padding:1px;">
+					<!-- <table class="tbl-wo-1" border="0" align="left">
 						<tr>
 							<td class="tbl-wo-data" valign="top">Requestor</td>
-							<td style="width:35%;">: <span style="color:blue;"><?= ($woinfo[0]->V_requestor) ? $woinfo[0]->V_requestor : 'NA' ?></span></td>
-							<td style="width:;">Designation</td>
-							<td style="width:30%;">: <span style="color:blue;"><?= ($woinfo[0]->V_MohDesg) ? $woinfo[0]->V_MohDesg : 'NA' ?></span></td>
+							<td style="width:33%;" valign="top">: <span style="color:blue;"><?= ($woinfo[0]->V_requestor) ? $woinfo[0]->V_requestor : 'NA' ?></span></td>
+							<td valign="top">Designation</td>
+							<td style="width:33%;">: <span style="color:blue;"><?= ($woinfo[0]->V_MohDesg) ? $woinfo[0]->V_MohDesg : 'NA' ?></span></td>
 						</tr>
 						<tr>
 							<td class="tbl-wo-data" valign="top">Department</td>
@@ -130,8 +139,76 @@
 							<td><span style="color:blue;">: <?= ($woinfo[0]->V_Serial_no) ? $woinfo[0]->V_Serial_no : 'NA' ?></span>
 							</td>
 						</tr>
-					</table></td>
-			<td style="width:50%; padding:1px;" valign="top" colspan="0"> 
+					</table> -->
+					<table class="tbl-wo-1" border="0" align="left">
+						<tr>
+							<td valign="top" width="50%">
+								<table class="tbl-wo-1">
+									<tr>
+										<td style="width:20%;" valign="top">Requestor</td>
+										<td style="width:50%;" valign="top">: <span style="color:blue;"><?= ($woinfo[0]->V_requestor) ? $woinfo[0]->V_requestor : 'NA' ?></span></td>
+									</tr>
+									<tr>
+										<td style="width:20%;" valign="top">Department</td>
+										<td valign="top" style=" width: 50%;">: <span style="color:blue;"><?= ($woinfo[0]->v_UserDeptDesc) ? $woinfo[0]->v_UserDeptDesc : 'NA' ?></span></td>
+									</tr>
+									<tr>
+										<td style="width:20%;">Dept. Code </td>
+										<td style=" width: 50%;"><span style="color:blue;">: <?= ($woinfo[0]->V_User_dept_code) ? $woinfo[0]->V_User_dept_code : 'NA' ?></span></td>
+									</tr>
+									<tr>
+										<td style="width:20%;">Room Code </td>
+										<td  style="width: 50%;"><span style="color:blue; ">: <?= ($woinfo[0]->V_location_code) ? $woinfo[0]->V_location_code : 'NA' ?> (<?= ($woinfo[0]->v_Location_Name) ? $woinfo[0]->v_Location_Name : 'NA' ?>)</span></td>
+									</tr>
+									<tr>
+										<td style="width:20%;">Equipment </td>
+										<td style=" width: 50%;"><span style="color:blue;">: <?= ($woinfo[0]->V_Asset_name) ? $woinfo[0]->V_Asset_name : 'NA' ?> </span></td>
+									</tr>
+								</table>
+							</td>
+							<td valign="top" width="50%">
+								<table class="tbl-wo-1">
+									<tr>
+										<td valign="top">Designation:</td>
+										<td> <span style="color:blue;"><?= ($woinfo[0]->V_MohDesg) ? $woinfo[0]->V_MohDesg : 'NA' ?></span></td>
+									</tr>
+									<tr>
+										<td valign="top">Ext. No:</td>
+										<td valign="top"> <span style="color:blue;"><?= ($woinfo[0]->V_phone_no) ? $woinfo[0]->V_phone_no : 'NA' ?></span></td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td valign="top" width="50%">
+								<table class="tbl-wo-1" width="100%">
+									<tr>
+										<td style="width: 45%">Model </td>
+										<td><span style="color:blue;">: <?= ($woinfo[0]->V_Model_no) ? $woinfo[0]->V_Model_no : 'NA' ?></span></td>
+									</tr>
+									<tr>
+										<td style="width: 27%">Serial No </td>
+										<td><span style="color:blue;">: <?= ($woinfo[0]->V_Serial_no) ? $woinfo[0]->V_Serial_no : 'NA' ?></span>
+										</td>
+									</tr>
+								</table>
+							</td>
+							<td valign="top" width="50%">
+								<table class="tbl-wo-1">
+									<tr>
+										<?php if(($this->session->userdata('usersess') == 'SEC') or ($this->session->userdata('usersess') == 'HKS')){?>
+
+										<?php }else{ ?>
+										<td valign="top">Asset No: </td>
+										<td valign="top"><span style="color:blue;"><?= ($woinfo[0]->V_Tag_no) ? $woinfo[0]->V_Tag_no : 'NA' ?></span></td>
+										<?php } ?>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</td>
+				<td style="width:50%; padding:1px;" valign="top" colspan="0"> 
 					<table class="tbl-wo-1" border="0" align="left">
 						<tr>
 							<td class="tbl-wo-data">Taken By </td>
@@ -174,18 +251,18 @@
 						
 						<?php }else{ ?>
 						<tr>
-						<td class="tbl-wo-data">Warranty Status </td>
-						<td> 
+						<td style="width: 29%">Warranty Status</td>
+						<td style="width: 69%"> 
 							<table class="tbl-wo" border="0" align="left">
 								<tr>
-									<td style="width:33.33%;"> : <div class="box2">
+									<td style="width:55%;"> : <div class="box2">
 									<?php if((strtotime($woinfo2[0]->V_Wrn_end_code) > strtotime(date("d/m/Y")))) {?>
 									<img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/>
 									<?php }else{ ?>
 									
 									<?php } ?>
 									</div> Under Warranty</td>
-									<td style="width:33.33%;"><div class="box2">
+									<td style="width:40%;"><div class="box2">
 									<?php  if(strtotime($woinfo2[0]->V_Wrn_end_code) < strtotime(date("d/m/Y"))) {?>
 									<img src="<?php echo base_url(); ?>images/tick2.png" class="img-tick"/>
 									<?php }else{ ?>
@@ -199,12 +276,16 @@
 						<?php } ?>
 					</table>
 				</td>
-				</tr>
-				<tr>
+			</tr>
+			<tr>
 				<td colspan="2" style="padding:1px;"> 
 					<table class="tbl-wo-1" border="0" align="bottom">
 						<tr>
-							<td> Request Description :</td>
+							<td> Request Description : 
+								<?php if(in_array($this->session->userdata("usersess"), array("FES", "BES")) ){?>
+								<span style="border-bottom: 1px dotted black;margin-right: 5px;width: 77%; height:12px; float: right;"></span>
+								<?php } ?>
+							</td>
 						</tr>
 						<?php if($woinfo[0]->V_summary == 'N/A'){?>
 						<tr>
@@ -215,14 +296,21 @@
 						</tr>
 						<?php }else{ ?>
 						<tr>
-							<td style="padding:3px 3px 3px 0px;"><span style="color:blue;"><?= ($woinfo[0]->V_summary) ? $woinfo[0]->V_summary : 'NA' ?></span></td>
+							<td style="padding:3px 3px 3px 0px;">
+								<p style="border-bottom: 1px dotted black; margin-left: 5px; margin-right: 5px;">
+									<span style="color:blue;"><?= ($woinfo[0]->V_summary) ? $woinfo[0]->V_summary : 'NA' ?></span>
+								</p>
+								<?php if(in_array($this->session->userdata("usersess"), array("SEC","HKS"))){?>
+								<p style="border-bottom: 1px dotted black; margin-left: 5px; margin-right: 5px;"></p>
+								<?php }?>
+							</td>
 						</tr>
 						<?php } ?>
 						
 						
 					</table> 
 				</td>
-				</tr>
+			</tr>
 		</table>
 		<table class="tbl-wo-1" border="0" align="center">
 			<tr>
@@ -245,16 +333,16 @@
 							<td> Response findings :</td>
 						</tr>
 						<tr>
-							<td><?=isset($resprec[0]->v_ActionTaken) ? $resprec[0]->v_ActionTaken : ''?> <hr class='dotted' style="margin:10px 5px 5px 5px;" /></td>
+							<td><p style="border-bottom: 1px dotted black; margin-left: 5px; margin-right: 5px;"><?=isset($resprec[0]->v_ActionTaken) ? $resprec[0]->v_ActionTaken : '&nbsp;'?> <!-- <hr class='dotted' style="margin:10px 5px 5px 5px;" /> --></p></td>
 						</tr>
 						<tr>
-							<td> <hr class='dotted' style="margin:10px 5px 5px 5px;" /></td>
+							<td><p style="border-bottom: 1px dotted black; margin-left: 5px; margin-right: 5px;"> &nbsp;</p></td>
 						</tr>
 						<?php if(($this->session->userdata('usersess') == 'SEC') or ($this->session->userdata('usersess') == 'HKS')){?>
 
 						<?php }else{ ?>
 						<tr>
-							<td> <hr class='dotted' style="margin:10px 5px 5px 5px;" /></td>
+							<td><p style="border-bottom: 1px dotted black; margin-left: 5px; margin-right: 5px;"> &nbsp;<!-- <hr class='dotted' style="margin:10px 5px 5px 5px;" /> --></p></td>
 						</tr>
 						<?php } ?>
 					</table> 
@@ -262,7 +350,7 @@
 				<td style="width:50%; padding:1px;" valign="top" style="" colspan="">
 					<table class="tbl-wo-1" border="0" align="left">
 						<tr>
-							<td colspan="3" height="30px" valign="top"> Requestor Verification : <br /> <i>(Name,Sign & Stamp)</i></td>
+							<td colspan="3" height="40px" valign="top"> Requestor Verification : <br /> <i>(Name,sign &amp; stamp)</i></td>
 						</tr>
 						<?php if(($this->session->userdata('usersess') == 'SEC') or ($this->session->userdata('usersess') == 'HKS')){?>
 						<tr>
@@ -286,15 +374,25 @@
 				<td style="width:50%; padding:1px;" valign="top" style="" colspan="">
 					<table class="tbl-wo-1" border="0" align="left">
 						<tr>
-							<td style="width:30%;" valign="top"> Equipment :</td>
-							<td style="width:33%;" valign="top"><div class="box2"></div> Remained on-side </td>
-							<td style="width:36%;"><div class="box2"></div> Released to Contractor <br/> 
-							<i style="padding-left:12px;">ETF Ref No : </i> </td>
+							<td>
+							<table class="tbl-wo-1" border="0" align="left">
+								<tr>
+									<td style="width:21%;" valign="top"> Equipment :</td>
+									<td style="width:33%;" valign="top"><div class="box2"></div> Remained on-side </td>
+									<td style="width:41%;"><div class="box2"></div> Released to Contractor <br/> 
+									<i style="padding-left:12px;">ETF Ref No : </i> </td>
+								</tr>
+							</table>
 						</tr>
 						<tr>
-							<td> Physical Condition :</td>
-							<td><div class="box2"></div> Good </td>
-							<td><div class="box2"></div> Not Good </td>
+							<td>
+							<table class="tbl-wo-1" border="0" align="left">
+								<tr>
+									<td style="width:41%;"> Physical Condition :</td>
+									<td style="width:21%;"><div class="box2"></div> Good </td>
+									<td style="width:33%;"><div class="box2"></div> Not Good </td>
+								</tr>
+							</table>
 						</tr>
 					</table>
 				</td>
@@ -430,9 +528,9 @@
 				<td style="width:50%; padding:0px 0px 0px 3px;" colspan="5" valign="top" heigth="10px" rowspan="">
 					<table class="tbl-wo" border="0" align="left">
 						<tr>
-							<td style="width:33.33%;"> Out-sourcing :</td>
-							<td style="width:33.33%;"><div class="box2"></div> Not Required</td>
-							<td style="width:33.33%;"><div class="box2"></div> Required</td>
+							<td style="width:30%;"> Out-sourcing :</td>
+							<td style="width:37%;"><div class="box2"></div> Not Required</td>
+							<td style="width:33%;"><div class="box2"></div> Required</td>
 						</tr>
 						<tr>
 							<td></td>
@@ -508,7 +606,7 @@
 							<td><div class="box2"></div> Not Required </td>
 							<?php } ?>
 						</tr>
-						<tr>
+						<tr style="line-height:33px;">
 							<td height="20px;" valign="bottom">Remarks : </td>
 							<td colspan="2" valign="bottom"><hr class='dotted' style="margin:5px 5px 5px 0px;"/></td>
 						</tr>
@@ -533,9 +631,9 @@
 			    <td style="width:50%; padding:0px 0px 0px 3px;" colspan="5">
 			    	<table class="tbl-wo" border="0" align="left">
 						<tr>
-							<td style="width:33.33%;">Claim Status :</td>
-							<td style="width:33.33%;"><div class="box2"></div> Not Reimbursable</td>
-							<td style="width:33.33%;"><div class="box2"></div> Reimbursable</td>
+							<td style="width:30%;">Claim Status :</td>
+							<td style="width:37%;"><div class="box2"></div> Not Reimbursable</td>
+							<td style="width:33%;"><div class="box2"></div> Reimbursable</td>
 						</tr>
 						<tr>
 							<td>Reference No :</td>
@@ -552,14 +650,28 @@
 							<td style="" colspan="3" valign="top" height="25px"> Recommendation / Report :</td>
 						</tr>
 						<tr>
-							<td style="width:33.33%;"><div class="box2"></div> Advisory Service</td>
-							<td style="width:33.33%;"><div class="box2"></div> BER</td>
-							<td style="width:33.33%;"><div class="box2"></div> Condition Appraisal</td>
+							<!-- <td style="width:34%;"><div class="box2"></div> Advisory Service</td>
+							<td style="width:13%;"><div class="box2"></div> BER</td>
+							<td style="width:98%;"><div class="box2"></div> Condition Appraisal</td> -->
+
+							<td style="width:37%;"><div class="box2"></div> Advisory Service</td>
+							<?php if(!in_array($this->session->userdata('usersess'), array('SEC','HKS'))){?>
+							<td style="width:15%;"><div class="box2"></div> BER</td>
+							<td style="width:70%;"><div class="box2"></div> Condition Appraisal</td>
+							<?php }else{?>
+							<td style="width:45%;" colspan="2"><div class="box2"></div> Condition Appraisal</td>
+							<?php }?>
 						</tr>
 						<tr>
-							<td><div class="box2"></div> Exemption</td>
-							<td align="right"> <i>Reference No :</i></td>
-							<td> <hr class='dotted' style="margin:margin:10px 10px 10px 0px;height:10px;"/></td>
+							<td colspan="3" width="100%">
+								<table class="tbl-wo" border="0" align="left" width="100%">
+									<tr>
+										<td width="37%"><div class="box2"></div> Exemption</td>
+										<td align="left" width="33%"> <i>Reference No :</i></td>
+										<td> <hr class='dotted' style="margin:margin:10px 10px 10px 0px;height:10px;"/></td>
+									</tr>
+								</table>
+							</td>
 						</tr>
 					</table>
 			    </td>
@@ -639,7 +751,7 @@
 			</tr>
 			<tr>
 				<td style="" valign="top"><span style="font-size:7px; padding-left:30px;"><i></i></span></td>
-				<td style="" valign="top" align="right"><span style="font-size:7px; padding-left:30px;"><i>Copyright product of Advance Pact Sdn Bhd. All rights reserved.</i></span></td>
+				<td style="" valign="top" align="right"><span style="font-size:7px; padding-left:30px;"><i>Copyright product of Advance Pact Sdn Bhd. All Right Reserveed.</i></span></td>
 			</tr>
 			<tr>
 				<td style="" valign="top"><span style="font-size:7px; padding-left:0px;">APSB-FORM</span></td>
@@ -652,7 +764,7 @@
 			<?php }else{ ?>
 			<tr>
 				<td style="" valign="top"><span style="font-size:7px; padding-left:30px;"><i>*Date and Time declared by APSB shall be deemed valid if NOT provided by the customer.</i></span></td>
-				<td style="" valign="top" align="right"><span style="font-size:7px; padding-left:30px;"><i>Copyright product of Advance Pact Sdn Bhd. All rights reserved.</i></span></td>
+				<td style="" valign="top" align="right"><span style="font-size:7px; padding-left:30px;"><i>Copyright product of Advance Pact Sdn Bhd. All Right Reserveed.</i></span></td>
 			</tr>
 			<?php } ?>
 		</table>
