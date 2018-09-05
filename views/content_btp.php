49e486b67bda8ea123341663f900fd83913d154b
BuzzLee93 <buzzlee199@yahoo>
2018-09-05 08:07:45 +0800

    delete old file
    for replace with file from git master

--- views/content_btp.php
+++
@@ -1,6 +0,0 @@
-<?php if($this->input->get('btp') == 1){
-$btp = $_SERVER['HTTP_REFERER'];
-}else{
-$btp = base_url().'index.php/contentcontroller/Schedule?m='.$this->input->get('m').'&y='.$this->input->get('y');
-}
-?>
--- views/content_vo3_tab_menu.php
+++
@@ -1,9 +0,0 @@
-<tr class="ui-left_web">
-<?= ($this->input->get('vff') == '0') ? '<td width="200px" class="ui-highlight" align="center" colspan="0" style="height:30px;">' : '<td width="200px" class="ui-content-menu-desk-color" align="center" colspan="0" style="height:30px;">' ?>
-<?php echo anchor ('contentcontroller/vo3_Analysis?rpt_no='.$this->input->get('rpt_no').'&'.$this->session->userdata('usersess').'&vff=0', 'Authorization Status'); ?></td>
-
-
-<?= ($this->input->get('vff') == '1') ? '<td width="200px" class="ui-highlight" align="center" colspan="0" style="">' : '<td width="200px" class="ui-content-menu-desk-color" align="center" colspan="0" style="">' ?>
-<?php echo anchor ('contentcontroller/vo3_als?rpt_no='.$this->input->get('rpt_no').'&'.$this->session->userdata('usersess').'&vff=1', 'Asset Lock Status'); ?></td>
-	<td>&nbsp;</td>
-</tr>
