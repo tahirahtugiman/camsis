03508349cd61e1ebf641287163dce91676ae8633
BuzzLee93 <buzzlee199@yahoo>
2018-10-08 10:02:37 +0800

    update table header
    make table header static while scroll data listing inside table

--- views/Content_report_rsls.php
+++ views/Content_report_rsls.php
@@ -1,110 +1,116 @@
 <?php
 if ($this->input->get('ex') == 'excel'){
-$filename ="Statutory & License Summary ".date('F', mktime(0, 0, 0, $month, 10)) .$year.".xls";
-header('Content-type: application/ms-excel');
-header('Content-Disposition: attachment; filename='.$filename);
+	$filename ="Statutory & License Summary ".date('F', mktime(0, 0, 0, $month, 10)) .$year.".xls";
+	header('Content-type: application/ms-excel');
+	header('Content-Disposition: attachment; filename='.$filename);
 }
 ?>
 <?php if ($this->input->get('ex') == ''){ ?>
-<div id="Instruction" class="pr-printer">
-    <div class="header-pr">Statutory & License Summary </div>
-    <button onclick="javascript:myFunction('report_rsls?m=<?=$month?>&y=<?=$year?>&none=closed&stat=<?php echo $this->input->get('stat');?>');" class="btn-button btn-primary-button">PRINT</button>
-    <button type="cancel" class="btn-button btn-primary-button" onclick="window.history.back()">CANCEL</button>
+	<div id="Instruction" class="pr-printer">
+		<div class="header-pr">Statutory &amp; License Summary </div>
+		<button onclick="javascript:myFunction('report_rsls?m=<?=$month?>&y=<?=$year?>&none=closed&stat=<?php echo $this->input->get('stat');?>');" class="btn-button btn-primary-button">PRINT</button>
+		<button type="cancel" class="btn-button btn-primary-button" onclick="window.history.back()">CANCEL</button>
 	<?php if (($this->input->get('ex') == '') or ($this->input->get('none') == '')){?>
-	<a href="<?php echo base_url();?>index.php/contentcontroller/report_rsls?m=<?=$this->input->get('m');?>&y=<?=$this->input->get('y');?>&ex=excel&none=close&stat=<?=$this->input->get('stat');?>&grp=<?=$this->input->get('grp');?>" style="float:right; margin-right:40px;"><img src="<?php echo base_url();?>images/excel.png" style="width:40px; height:38px; position:absolute;" title="export to excel"></a>
-	<a href="<?php echo base_url();?>index.php/contentcontroller/report_rsls?m=<?=$this->input->get('m');?>&y=<?=$this->input->get('y');?>&pdf=1&stat=<?=$this->input->get('stat');?>&grp=<?=$this->input->get('grp');?>" style="float:right; margin-right:80px;"><img src="<?php echo base_url();?>images/pdf.png" style="width:40px; height:38px; position:absolute;" title="export to pdf"></a>
+		<a href="<?php echo base_url();?>index.php/contentcontroller/report_rsls?m=<?=$this->input->get('m');?>&y=<?=$this->input->get('y');?>&ex=excel&none=close&stat=<?=$this->input->get('stat');?>&grp=<?=$this->input->get('grp');?>" style="float:right; margin-right:40px;"><img src="<?php echo base_url();?>images/excel.png" style="width:40px; height:38px; position:absolute;" title="export to excel"></a>
+		<a href="<?php echo base_url();?>index.php/contentcontroller/report_rsls?m=<?=$this->input->get('m');?>&y=<?=$this->input->get('y');?>&pdf=1&stat=<?=$this->input->get('stat');?>&grp=<?=$this->input->get('grp');?>" style="float:right; margin-right:80px;"><img src="<?php echo base_url();?>images/pdf.png" style="width:40px; height:38px; position:absolute;" title="export to pdf"></a>
 	<?php } ?>
 	</div>
 <?php } ?>
 <div class="menu" style="position:relative;">
-<?php if (($this->input->get('ex') == '') or ($this->input->get('none') == 'closed')){?>
-<?php include 'content_headprint.php';?>
-<?php } ?>
+	<?php if (($this->input->get('ex') == '') or ($this->input->get('none') == 'closed')){?>
+		<?php include 'content_headprint.php';?>
+	<?php } ?>
 
-<div class="m-div">
-	<table class="rport-header">
-		<tr>
-			<td>Statutory & License Summary <?=date('F', mktime(0, 0, 0, $month, 10))?> <?=$year?><br><i>Computer Generated - CAMSIS</i></td>
-		</tr>
-	</table>
-	<table class="tftable" border="1" style="text-align:center;">
-		<tr>
-			<th>No</th>
-			<th>Certificate No</th>
-			<th>Agency Code</th>
-			<th>License Category Code</th>
-			<th>Asset No</th>	
-			<th>Location</th>			
-			<th>Identification</th>
-			<th>Start Date</th>
-			<th>Expiry Date</th>
-			<th>Grade ID</th>
-			<th>Remarks</th>
-			<th>Image</th>
-		</tr>
-		<tr style="text-align:center;">								
-			
-		</tr>
-		<?php $numrow = 1; ?>
-		<?php  if (!empty($record2) || !empty($record)) {?>
-				<?php  foreach($record2 as $row):?>
-					      			
-	    				<?php echo ($numrow%2==0) ? '<tr class="ui-color-color-color">' : '<tr>'; ?>
-	    					
-    		<td><span style="color:#0000FF;"><?= $numrow ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_cert_no) ? $row->v_cert_no : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_authority) ? $row->v_authority : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_authority) ? $row->v_authority : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->V_Tag_no) ? $row->V_Tag_no : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->V_Location_code) ? $row->v_Location_Name.' <br>('.$row->V_Location_code.')' : 'N/A'?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_authority) ? $row->v_authority : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->D_start) ? date("d-m-Y",strtotime($row->D_start)): 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->D_end) ? date("d-m-Y",strtotime($row->D_end)) : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_GradeID) ? $row->v_GradeID : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_Remarks) ? $row->v_Remarks : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><img src="<?php echo base_url(); ?>uploadlicenseimages/No_image_available.jpg" style="width:15px; height:15px;" onclick="javascript:image('<?php echo base_url(); ?>uploadlicenseimages/No_image_available.jpg');"/></span></td>
-  
-	        			</tr>	
-	        			<?php $numrow++; ?>
-			    		<?php endforeach;?>
-			    		<?php } ?>
-			<?php  if (!empty($record) || !empty($record2)) {?>
-				<?php  foreach($record as $row):?>
-					      			
-	    				<?php echo ($numrow%2==0) ? '<tr class="ui-color-color-color">' : '<tr>'; ?>
-	    					
-    		<td><span style="color:#0000FF;"><?= $numrow ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_CertificateNo) ? $row->v_CertificateNo : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_AgencyCode) ? $row->v_AgencyCode : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_LicenseCategoryCode) ? $row->v_LicenseCategoryCode : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_registrationno) ? $row->v_registrationno : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->V_Location_code) ? $row->v_Location_Name.' <br>('.$row->V_Location_code.')' : 'N/A'?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_Identification) ? $row->v_Identification : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_StartDate) ? date("d-m-Y",strtotime($row->v_StartDate)): 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_ExpiryDate) ? date("d-m-Y",strtotime($row->v_ExpiryDate)) : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_GradeID) ? $row->v_GradeID : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><?= ($row->v_Remarks) ? $row->v_Remarks : 'N/A' ?></span></td>
-			<td><span style="color:#0000FF;"><img src="<?php echo base_url(); ?>uploadlicenseimages/<?=isset($row->file_name) ? $row->file_name : 'No_image_available.jpg'?>" style="width:15px; height:15px;" onclick="javascript:image('<?php echo base_url(); ?>uploadlicenseimages/<?=isset($row->file_name) ? $row->file_name : 'No_image_available.jpg'?>');"/></span></td>
-	        			</tr>	
-	        			<?php $numrow++; ?>
-			    		<?php endforeach;?>
-			    		<?php }else { ?>
-						<tr align="center" style="background:white; height:200px;">
-	    					<td colspan="14"><span style="color:red;">NO COMPLAINT RECORDS FOUND FOR THIS WORK ORDER.</span></td>
-	    				</tr>
-						<?php } ?>
-			
 
-	</table>
-	<!-- The Modal -->
-	
-<?php if( $this->input->get('ex') == ''){?>
-	<div id="myModal" class="modal">
-	  <span class="close">&times;</span>
-	  <img class="modal-content" id="img01">
-	  <div id="caption"></div>
+	<div class="m-div">
+		<table class="rport-header">
+			<tr>
+				<td>Statutory &amp; License Summary <?=date('F', mktime(0, 0, 0, $month, 10))?> <?=$year?><br><i>Computer Generated - CAMSIS</i></td>
+			</tr>
+		</table>
+
+		<?php if ($this->input->get('none') == 'closed') {?>
+		<table class="tftable" border="1" style="text-align:center;">
+		<?php }else{ ?>
 	</div>
 </div>
+
+<div id="constrainer">
+	<div class="scrolltable1">
+		<table class="header-alr" style="text-align:center; font-size: 12px;">
+		<?php } ?>
+			<tr>
+				<th>No</th>
+				<th>Certificate No</th>
+				<th>Agency Code</th>
+				<th>License Category Code</th>
+				<th>Asset No</th>	
+				<th>Location</th>			
+				<th>Identification</th>
+				<th>Start Date</th>
+				<th>Expiry Date</th>
+				<th>Grade ID</th>
+				<th>Remarks</th>
+				<th>Image</th>
+			</tr>
+			<tr style="text-align:center;"></tr>
+			<?php $numrow = 1; ?>
+			<?php  if (!empty($record2) || !empty($record)) {?>
+				<?php  foreach($record2 as $row):?>
+					<?php echo ($numrow%2==0) ? '<tr class="ui-color-color-color">' : '<tr>'; ?>
+
+				<td><span style="color:#0000FF;"><?= $numrow ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_cert_no) ? $row->v_cert_no : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_authority) ? $row->v_authority : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_authority) ? $row->v_authority : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->V_Tag_no) ? $row->V_Tag_no : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->V_Location_code) ? $row->v_Location_Name.' <br>('.$row->V_Location_code.')' : 'N/A'?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_authority) ? $row->v_authority : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->D_start) ? date("d-m-Y",strtotime($row->D_start)): 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->D_end) ? date("d-m-Y",strtotime($row->D_end)) : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_GradeID) ? $row->v_GradeID : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_Remarks) ? $row->v_Remarks : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><img src="<?php echo base_url(); ?>uploadlicenseimages/No_image_available.jpg" style="width:15px; height:15px;" onclick="javascript:image('<?php echo base_url(); ?>uploadlicenseimages/No_image_available.jpg');"/></span></td>
+
+			</tr>	
+					<?php $numrow++; ?>
+				<?php endforeach;?>
+			<?php } ?>
+			<?php  if (!empty($record) || !empty($record2)) {?>
+				<?php  foreach($record as $row):?>
+			<?php echo ($numrow%2==0) ? '<tr class="ui-color-color-color">' : '<tr>'; ?>
+								
+				<td><span style="color:#0000FF;"><?= $numrow ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_CertificateNo) ? $row->v_CertificateNo : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_AgencyCode) ? $row->v_AgencyCode : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_LicenseCategoryCode) ? $row->v_LicenseCategoryCode : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_registrationno) ? $row->v_registrationno : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->V_Location_code) ? $row->v_Location_Name.' <br>('.$row->V_Location_code.')' : 'N/A'?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_Identification) ? $row->v_Identification : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_StartDate) ? date("d-m-Y",strtotime($row->v_StartDate)): 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_ExpiryDate) ? date("d-m-Y",strtotime($row->v_ExpiryDate)) : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_GradeID) ? $row->v_GradeID : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><?= ($row->v_Remarks) ? $row->v_Remarks : 'N/A' ?></span></td>
+				<td><span style="color:#0000FF;"><img src="<?php echo base_url(); ?>uploadlicenseimages/<?=isset($row->file_name) ? $row->file_name : 'No_image_available.jpg'?>" style="width:15px; height:15px;" onclick="javascript:image('<?php echo base_url(); ?>uploadlicenseimages/<?=isset($row->file_name) ? $row->file_name : 'No_image_available.jpg'?>');"/></span></td>
+			</tr>	
+				<?php $numrow++; ?>
+				<?php endforeach;?>
+				<?php }else { ?>
+			<tr align="center" style="background:white; height:200px;">
+				<td colspan="14"><span style="color:red;">NO COMPLAINT RECORDS FOUND FOR THIS WORK ORDER.</span></td>
+			</tr>
+			<?php } ?>
+			
+		</table>
+		<!-- The Modal -->
+	
+		<?php if( $this->input->get('ex') == ''){?>
+		<div id="myModal" class="modal">
+			<span class="close">&times;</span>
+			<img class="modal-content" id="img01">
+			<div id="caption"></div>
+		</div>
+	</div>
 	<table width="99%" border="0">
 		<tr>
 			<td valign="top" colspan="2"><hr color="black" size="1Px"></td>
@@ -116,7 +122,7 @@ header('Content-Disposition: attachment; filename='.$filename);
 	</table>
 </div>
 <?php if (($this->input->get('ex') == '') or ($this->input->get('none') == 'closed')){?>
-<?php include 'content_footerprint.php';?>
+	<?php include 'content_footerprint.php';?>
 <?php } ?>
 <div class="StartNewPage" id="breakpage"><span id="pagebreak">Page Break</span></div>
 
